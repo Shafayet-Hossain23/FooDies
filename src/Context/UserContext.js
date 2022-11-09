e4b2a7eb9fca574c923058b26c5510cac76ab1ba
Shafayet-Hossain23 <mdshafayethossain6530@gmail.com>
2022-11-09 08:40:53 +0600

    userContext-setup

---
+++ src/Context/UserContext.js
@@ -0,0 +1,60 @@
+import React, { createContext, useEffect, useState } from 'react';
+import { createUserWithEmailAndPassword, getAuth, GithubAuthProvider, onAuthStateChanged, signInWithEmailAndPassword, signInWithPopup, signOut, updateProfile } from "firebase/auth";
+import { GoogleAuthProvider } from "firebase/auth";
+import app from '../Firebase/firebase.config';
+
+export const AuthContext = createContext(null);
+const auth = getAuth(app)
+const googleProvider = new GoogleAuthProvider();
+const githubProvider = new GithubAuthProvider();
+
+const UserContext = ({ children }) => {
+    const [user, setUser] = useState(null)
+    const [loading, setLoading] = useState(true)
+    const registerHandler = (email, password) => {
+        setLoading(true)
+        return createUserWithEmailAndPassword(auth, email, password)
+    }
+    const loginHandler = (email, password) => {
+        setLoading(true)
+        return signInWithEmailAndPassword(auth, email, password)
+    }
+    const googlePopUpSignin = () => {
+        setLoading(true)
+        return signInWithPopup(auth, googleProvider)
+    }
+    const logoutHandler = () => {
+        setLoading(true)
+        return signOut(auth)
+    }
+    const updateProfileHandler = (name, url) => {
+        setLoading(true)
+        return updateProfile(auth.currentUser, {
+            displayName: name,
+            photoURL: url
+        })
+    }
+    const githubPopup = () => {
+        setLoading(true)
+        return signInWithPopup(auth, githubProvider)
+    }
+    useEffect(() => {
+        const unsubscribe = onAuthStateChanged(auth, (currentUser) => {
+
+            setUser(currentUser)
+            setLoading(false)
+        });
+        return () => {
+            unsubscribe()
+        }
+    }, [])
+
+    const userInfo = { user, loading, registerHandler, loginHandler, googlePopUpSignin, updateProfileHandler, githubPopup, logoutHandler }
+    return (
+        <AuthContext.Provider value={userInfo}>
+            {children}
+        </AuthContext.Provider>
+    );
+};
+
+export default UserContext;
---
+++ src/Routes/PrivateRoute.js
@@ -0,0 +1,27 @@
+import React, { useContext } from 'react';
+
+import { Navigate, useLocation } from 'react-router-dom';
+import { AuthContext } from '../Context/UserContext';
+
+
+const PrivateRoute = ({ children }) => {
+    const { user, loading } = useContext(AuthContext)
+    const location = useLocation()
+    if (loading) {
+        return <>
+            <button type="button" className="text-secondary text-center" disabled>
+                <svg className="animate-spin h-5 w-5 mr-3 " viewBox="0 0 24 24">
+                </svg>
+                Loading...
+            </button>
+        </>
+    }
+
+    if (!user) {
+        return <Navigate to="/login" state={{ from: location }} replace></Navigate>
+    }
+    return children
+
+};
+
+export default PrivateRoute;
